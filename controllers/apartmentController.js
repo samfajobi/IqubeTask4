4045b7149f906f2b1f317babbb34e0096836b189
Sam Fajobi <segzyfaj1@gmail.com>
2023-02-01 02:04:40 +0100

    Added GetApartmentById Schema

--- controllers/apartmentController.js
+++ controllers/apartmentController.js
@@ -11,9 +11,23 @@ import Product from '../models/productModel.js'
  })
 
 
+ // @desc    Fetch single product
+ // @route   GET /api/products/:id
+ // @access  Public
+ const getApartmentById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+ 
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+
 
  
 
 
 
-export { getApartments, getProductById,  createProductReview }
+export { getApartments, getApartmentById,  createProductReview }
