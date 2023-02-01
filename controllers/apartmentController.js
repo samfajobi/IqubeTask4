803e12376d89c15332fa8bda741f75c1bbe785b6
Sam Fajobi <segzyfaj1@gmail.com>
2023-02-01 02:03:26 +0100

    Added GetApartment Controller

---
+++ controllers/apartmentController.js
@@ -0,0 +1,19 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+ // @desc    Fetch all products
+ // @route   GET /api/products
+ // @access  Public
+ const getApartments = asyncHandler(async (req, res) => {
+   const products = await Product.find({})
+
+   res.json(products)
+ })
+
+
+
+ 
+
+
+
+export { getApartments, getProductById,  createProductReview }
